bd446d786d51ffad0c897beacbd9cff95e771a7b
Drew Stephens <drew@dinomite.net>
2013-02-04 10:06:40 -0500

    Only show legend for displayed lines

--- pricing.r
+++ pricing.r
@@ -52,23 +52,37 @@ break_3year_heavy_x = reserve_3year_heavy / (on_demand_daily - reserve_3year_hea
 png(filename = "ec2_m1large_cost.png", width = width, height = height)
 plot(x,y, type="l", col='red', xlab="", ylab="cost ($USD)")
 
+# Title with summary information
 title("EC2 cost analysis for m1.large",
       sprintf("(days)\nLight Utilization: 1 year is cheaper than on-demand after %.0f days, 3 year after %.0f days\nMedium Utilization: 1 year is cheaper than on-demand after %.0f days, 3 year after %.0f days\nHeavy Utilization: 1 year is cheaper than on-demand after %.0f days, 3 year after %.0f days", break_1year_light_x, break_3year_light_x, break_1year_medium_x, break_3year_medium_x, break_1year_heavy_x, break_3year_heavy_x))
 
+# Arrays for legend
+legend_names <- c(sprintf("On Demand ($%.2f/hour)", on_demand_hourly))
+legend_colors <- c("red")
+legend_line_types <- c(1)
 
 # Light utilization
-    light_color="green"
+light_color="green"
 if (light) {
     abline(reserve_1year_light, reserve_1year_light_daily, lty=1, lwd=line_thickness, col=light_color)
     point_y = reserve_1year_light + reserve_1year_light_daily * break_1year_light_x
     points(break_1year_light_x, point_y)
     text(break_1year_light_x, point_y, labels = sprintf("%.0f days", break_1year_light_x), pos=1)
 
+    legend_names <- cbind(legend_names, sprintf("1 year light utilization ($%.0f+$%.2f/hour)", reserve_1year_light, reserve_1year_light_hourly))
+    legend_colors <- cbind(legend_colors, light_color)
+    legend_line_types <- cbind(legend_line_types, 1)
+
+
     if (three_year) {
         abline(reserve_3year, reserve_3year_light_daily, lty=2, lwd=line_thickness, col=light_color)
         point_y = reserve_3year + reserve_3year_light_daily * break_3year_light_x
         points(break_3year_light_x, point_y)
         text(break_3year_light_x, point_y, labels = sprintf("%.0f days", break_3year_light_x), pos=1)
+
+        legend_names <- cbind(legend_names, sprintf("3 year light utilization ($%.0f+$%.2f/hour)", reserve_3year_light, reserve_3year_light_hourly))
+        legend_colors <- cbind(legend_colors, light_color)
+        legend_line_types <- cbind(legend_line_types, 2)
     }
 }
 
@@ -80,11 +94,19 @@ if (medium) {
     points(break_1year_medium_x, point_y)
     text(break_1year_medium_x, point_y, labels = sprintf("%.0f days", break_1year_medium_x), pos=1)
 
+    legend_names <- cbind(legend_names, sprintf("1 year medium utilization ($%.0f+$%.2f/hour)", reserve_1year_medium, reserve_1year_medium_hourly))
+    legend_colors <- cbind(legend_colors, medium_color)
+    legend_line_types <- cbind(legend_line_types, 1)
+
     if (three_year) {
         abline(reserve_3year, reserve_3year_medium_daily, lty=2, lwd=line_thickness, col=medium_color)
         point_y = reserve_3year + reserve_3year_medium_daily * break_3year_medium_x
         points(break_3year_medium_x, point_y)
         text(break_3year_medium_x, point_y, labels = sprintf("%.0f days", break_3year_medium_x), pos=1)
+
+        legend_names <- cbind(legend_names, sprintf("3 year medium utilization ($%.0f+$%.2f/hour)", reserve_3year_medium, reserve_3year_medium_hourly))
+        legend_colors <- cbind(legend_colors, medium_color)
+        legend_line_types <- cbind(legend_line_types, 2)
     }
 }
 
@@ -96,26 +118,23 @@ if (heavy) {
     points(break_1year_heavy_x, point_y)
     text(break_1year_heavy_x, point_y, labels = sprintf("%.0f days", break_1year_heavy_x), pos=1)
 
+    legend_names <- cbind(legend_names, sprintf("1 year heavy utilization ($%.0f+$%.2f/hour)", reserve_1year_heavy, reserve_1year_heavy_hourly))
+    legend_colors <- cbind(legend_colors, heavy_color)
+    legend_line_types <- cbind(legend_line_types, 1)
+
     if (three_year) {
         abline(reserve_3year, reserve_3year_heavy_daily, lty=2, lwd=line_thickness, col=heavy_color)
         point_y = reserve_3year + reserve_3year_heavy_daily * break_3year_heavy_x
         points(break_3year_heavy_x, point_y)
         text(break_3year_heavy_x, point_y, labels = sprintf("%.0f days", break_3year_heavy_x), pos=1)
+
+        legend_names <- cbind(legend_names, sprintf("3 year heavy utilization ($%.0f+$%.2f/hour)", reserve_3year_heavy, reserve_3year_heavy_hourly))
+        legend_colors <- cbind(legend_colors, heavy_color)
+        legend_line_types <- cbind(legend_line_types, 2)
     }
 }
 
 # Legend
-legend_names <- c(
-sprintf("On Demand ($%.2f/hour)", on_demand_hourly),
-sprintf("1 year light utilization ($%.0f+$%.2f/hour)", reserve_1year_light, reserve_1year_light_hourly),
-sprintf("3 year light utilization ($%.0f+$%.2f/hour)", reserve_3year_light, reserve_3year_light_hourly),
-sprintf("1 year medium utilization ($%.0f+$%.2f/hour)", reserve_1year_medium, reserve_1year_medium_hourly),
-sprintf("3 year medium utilization ($%.0f+$%.2f/hour)", reserve_3year_medium, reserve_3year_medium_hourly),
-sprintf("1 year heavy utilization ($%.0f+$%.2f/hour)", reserve_1year_heavy, reserve_1year_heavy_hourly),
-sprintf("3 year heavy utilization ($%.0f+$%.2f/hour)", reserve_3year_heavy, reserve_3year_heavy_hourly)
-)
-legend_colors <- c("red", light_color, light_color, medium_color, medium_color, heavy_color, heavy_color)
-legend_line_types <- c(1,1,2,1,2,1,2)
 legend("topleft", legend_names, cex=1.2, col=legend_colors, lty=legend_line_types, lwd=line_thickness, bty="n")
 
 dev.off()
